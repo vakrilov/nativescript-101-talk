fa04da0afd7be6fcf1cdca23bec74f437f2e627e
sis0k0 <stanimira.vlaeva@gmail.com>
2017-11-13 19:23:42 +0200

    refactor: add big data method to MorseService

--- PlaygroundMorseCode/app/morse.service.ts
+++ PlaygroundMorseCode/app/morse.service.ts
@@ -13,60 +13,6 @@ const morseCode = {
     "6": "_....", "7": "__...", "8": "___..", "9": "____.", "0": "_____"
 }
 
-@Injectable()
-export class MorseService {
-    public translate(message: string): string {
-        return message.split("")
-            .map(char => morseCode[char.toLocaleLowerCase()])
-            .join(" ");
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-// public translate(msg: string): string {
-//     return msg.split("")
-//         .map(char => morseCode[char.toLocaleLowerCase()])
-//         .join(" ");
-// }
-
-// public getBigData(msg: string): BigData {
-//     const code = this.translate(msg);
-//     const res = {};
-//     const dotSeries: DataPoint[] = [{ name: "0", value: 0 }];
-//     const dashSeries: DataPoint[] = [{ name: "0", value: 0 }];
-//     let count = 1;
-
-//     code.split('').forEach((char, i, arr) => {
-//         res[char] = res[char] ? res[char] + 1 : 1;
-//         if (char === " " || i === (arr.length - 1)) {
-//             dotSeries.push({ name: count + "", value: res["."] || 0 });
-//             dashSeries.push({ name: count + "", value: res["_"] || 0 });
-//             count++;
-//         }
-//     });
-
-//     const pieData = [
-//         { name: "dot", value: res["."] },
-//         { name: "dash", value: res["_"] },
-//         { name: "space", value: res[" "] }
-//     ];
-
-//     return { pieData, dotSeries, dashSeries };
-// }
-
-
-
 export interface DataPoint {
     name: string;
     value: number;
@@ -76,5 +22,40 @@ export interface BigData {
     pieData: DataPoint[];
     dotSeries: DataPoint[];
     dashSeries: DataPoint[];
+}
+
+
+@Injectable()
+export class MorseService {
+    public translate(message: string): string {
+        return message.split("")
+            .map(char => morseCode[char.toLocaleLowerCase()])
+            .join(" ");
+    }
+
+    public getBigData(msg: string): BigData {
+        const code = this.translate(msg);
+        const res = {};
+        const dotSeries: DataPoint[] = [{ name: "0", value: 0 }];
+        const dashSeries: DataPoint[] = [{ name: "0", value: 0 }];
+        let count = 1;
+
+        code.split("").forEach((char, i, arr) => {
+            res[char] = res[char] ? res[char] + 1 : 1;
+            if (char === " " || i === (arr.length - 1)) {
+                dotSeries.push({ name: count + "", value: res["."] || 0 });
+                dashSeries.push({ name: count + "", value: res["_"] || 0 });
+                count++;
+            }
+        });
+
+        const pieData = [
+            { name: "dot", value: res["."] },
+            { name: "dash", value: res["_"] },
+            { name: "space", value: res[" "] }
+        ];
+
+        return { pieData, dotSeries, dashSeries };
+    }
 }
 
